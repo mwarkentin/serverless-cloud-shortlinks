6530884beee1c500e7a7a493ff48177451cbb8a5
Michael Warkentin <mwarkentin@hey.com>
2022-02-04 12:24:18 -0500

    prettier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Buffer } from 'buffer';
+import { Buffer } from "buffer";
 
 import { api, data } from "@serverless/cloud";
 
@@ -6,18 +6,20 @@ api.get("/", async (req, res) => {
   res.send("<h1>Hello Serverless Cloud!</h1>");
 });
 
-api.get('/l/:shortUrl', async (req,res) => {
+api.get("/l/:shortUrl", async (req, res) => {
   let result = await data.get(req.params.shortUrl);
-  console.log("Result", result)
-  res.send("Short URL: " + req.params.shortUrl + "<br><br>Long URL: " + result)
-})
+  console.log("Result", result);
+  res.send("Short URL: " + req.params.shortUrl + "<br><br>Long URL: " + result);
+});
 
 // TODO: Switch to POST
-api.get('/s/:longUrl', async (req,res) => {
+api.get("/s/:longUrl", async (req, res) => {
   let bufferObj = Buffer.from(req.params.longUrl, "utf8");
   let shortenedUrl = bufferObj.toString("base64");
   console.log("The encoded base64 string is:", shortenedUrl);
   await data.set(shortenedUrl, req.params.longUrl);
 
-  res.send("Long URL: " + req.params.longUrl + "<br><br>Short URL: " + shortenedUrl)
-})
+  res.send(
+    "Long URL: " + req.params.longUrl + "<br><br>Short URL: " + shortenedUrl
+  );
+});
--- tests/integration/api.test.js
+++ tests/integration/api.test.js
@@ -4,3 +4,17 @@ test("should return Hello Serverless Cloud!", async () => {
   const { body } = await api.get("/").invoke();
   expect(body).toBe("<h1>Hello Serverless Cloud!</h1>");
 });
+
+test("save longlinks", async () => {
+  const { body } = await api.get("/s/www.google.com").invoke();
+  expect(body).toBe(
+    "Long URL: www.google.com<br><br>Short URL: d3d3Lmdvb2dsZS5jb20="
+  );
+});
+
+test("get shortlink", async () => {
+  const { body } = await api.get("/l/d3d3Lmdvb2dsZS5jb20=").invoke();
+  expect(body).toBe(
+    "Short URL: d3d3Lmdvb2dsZS5jb20=<br><br>Long URL: www.google.com"
+  );
+});
