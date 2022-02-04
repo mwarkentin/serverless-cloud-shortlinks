0973974c3f35ec8efd9be5c75b7e6b5dc4b32d1e
Michael Warkentin <mwarkentin@hey.com>
2022-02-04 12:36:51 -0500

    string interpolation and 302 redirect response

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ api.get("/", async (req, res) => {
 
 api.get("/l/:shortUrl", async (req, res) => {
   let result = await data.get(req.params.shortUrl);
-  console.log("Result", result);
-  res.send("Short URL: " + req.params.shortUrl + "<br><br>Long URL: " + result);
+  let fullUrl = `https://${result}`;
+  console.log(`Short URL: ${req.params.shortUrl}, Long URL: ${fullUrl}`);
+  res.redirect(fullUrl);
 });
 
 // TODO: Switch to POST
 api.get("/s/:longUrl", async (req, res) => {
   let bufferObj = Buffer.from(req.params.longUrl, "utf8");
   let shortenedUrl = bufferObj.toString("base64");
-  console.log("The encoded base64 string is:", shortenedUrl);
+  console.log(`The encoded base64 string is: ${shortenedUrl}`);
   await data.set(shortenedUrl, req.params.longUrl);
 
-  res.send(
-    "Long URL: " + req.params.longUrl + "<br><br>Short URL: " + shortenedUrl
-  );
+  res.send(`Long URL: ${req.params.longUrl}<br><br>Short URL: ${shortenedUrl}`);
 });
