a9b90a7e47e61d4ae411ce8bde55424db35f15db
Michael Warkentin <michael@X-0111.local>
2022-02-10 21:55:16 -0500

    add click tracking

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ import cors from "cors";
 api.use(cors());
 
 api.get("/l/:shortUrl", async (req, res) => {
-  let result = await data.get(req.params.shortUrl);
-  let fullUrl = `https://${result}`;
-  console.log(`Short URL: ${req.params.shortUrl}, Long URL: ${fullUrl}`);
+  let shortUrl = req.params.shortUrl;
+  var result = await data.get(shortUrl);
+  result.clicks++
+  let updatedObj = await data.set(shortUrl, { clicks: result.clicks });
+  let fullUrl = `https://${result.url}`;
+  console.log(`Short URL: ${shortUrl}, Updated Obj: ${JSON.stringify(updatedObj)}`);
   res.redirect(fullUrl);
 });
 
@@ -17,7 +20,7 @@ api.post("/s/:longUrl", async (req, res) => {
   let bufferObj = Buffer.from(req.params.longUrl, "utf8");
   let shortenedUrl = bufferObj.toString("base64");
   console.log(`The encoded base64 string is: ${shortenedUrl}`);
-  await data.set(shortenedUrl, req.params.longUrl);
+  let result = await data.set(shortenedUrl, { url: req.params.longUrl, clicks: 0 });
 
-  res.send(`Long URL: ${req.params.longUrl}<br><br>Short URL: ${shortenedUrl}`);
+  res.send(`Result: ${JSON.stringify(result)}<br><br>Short URL: ${shortenedUrl}`);
 });
